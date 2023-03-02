5c23c3ab2112db00ba183a3b038babd2c7853f9c
amittyagi7 <amit.tyagi7@gmail.com>
2023-03-02 22:31:18 +0530

    afer final merging branch1 and branch2

--- src/nsaur/in/Test1.java
+++ src/nsaur/in/Test1.java
@@ -5,13 +5,12 @@ public class Test1 {
 	public static void main(String[] args) {
 
 		System.out.println("It is test 1 class");
-<<<<<<< HEAD
+
 		System.out.println("It is test 1.1 class from local");
 		System.out.println("It is test 1.2 class from local");//yes
-=======
+
 		System.out.println("It is test 1.3 class from local branch2");
 		
->>>>>>> branch2
 		
 
 	}
