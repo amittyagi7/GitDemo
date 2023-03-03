842e83f72f757df9ea74e3070912781f90d8e08a
amittyagi7 <amit.tyagi7@gmail.com>
2023-03-03 15:35:29 +0530

    from branch 3

--- src/nsaur/in/Test3.java
+++ src/nsaur/in/Test3.java
@@ -3,7 +3,8 @@ package nsaur.in;
 public class Test3 {
 
 	public static void main(String[] args) {
-		System.out.println("Test3 3.1 from branch3");
+		System.out.println("Test3 3.1 from main");
+		System.out.println("Test3 3.2 from branch3");
 
 	}
 
