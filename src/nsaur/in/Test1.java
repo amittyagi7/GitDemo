970fc8c4fff3bf1b53a91101856cd80edf353af7
amittyagi7 <amit.tyagi7@gmail.com>
2023-03-16 09:44:22 +0530

    from branch1 test1

--- src/nsaur/in/Test1.java
+++ src/nsaur/in/Test1.java
@@ -8,7 +8,6 @@ public class Test1 {
 
 		System.out.println("It is test 1.1 class from local");
 		System.out.println("It is test 1.2 class from local");//yes
-
 		System.out.println("It is test 1.3 class from local branch2");
 		
 		
